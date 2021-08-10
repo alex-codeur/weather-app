eb8a7effbaa0d58f1bcf8f1f47db04143d4da78f
alex-codeur <apachecordovax@gmail.com>
2021-08-10 13:17:59 +0200

    firebase: insertion des donnees

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,52 @@
 
 <script>
 import axios from "axios";
+import db from "./firebase/firebaseinit";
 export default {
   name: "App",
   data() {
     return {
       APIKey: "d6679beaa23bbcc909a3ed8692b18a4f",
       city: "Douala",
+      cities: [],
     };
   },
   created() {
-    this.getCurrentWeather();
+    this.getCityWeather();
   },
   methods: {
+    getCityWeather() {
+      let firebaseDB = db.collection("cities");
+
+      firebaseDB.onSnapshot((snap) => {
+        snap.docChanges().forEach(async (doc) => {
+          if (doc.type === "added") {
+            try {
+              const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${
+                  doc.doc.data().city
+                }&appid=${this.APIKey}`
+              );
+
+              const data = response.data;
+              firebaseDB
+                .doc(doc.doc.id)
+                .update({
+                  currentWeather: data,
+                })
+                .then(() => {
+                  this.cities.push(doc.doc.data());
+                })
+                .then(() => {
+                  console.log(this.cities);
+                });
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        });
+      });
+    },
     getCurrentWeather() {
       axios
         .get(
