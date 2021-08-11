f64cd4f164f81afb2f4e8327636d71669d8d3ae8
alex-codeur <apachecordovax@gmail.com>
2021-08-11 10:23:08 +0200

    listing des villes

--- src/views/AddCity.vue
+++ src/views/AddCity.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="home"></div>
+  <div class="grid">
+    <div class="city-link" v-for="(city, index) in cities" v-bind:key="index">
+      <City v-bind:city="city" />
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import City from "../components/City.vue";
 export default {
   name: "AddCity",
-  components: {},
+  props: ["cities"],
+  created() {
+    console.log(this.cities);
+  },
+  components: {
+    City,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  padding-top: 81px;
+  background-color: #31363d;
+  width: 100%;
+  min-height: 100vh;
+  @media (min-width: 400px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
